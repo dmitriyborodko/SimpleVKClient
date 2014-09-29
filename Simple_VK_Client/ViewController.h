f4f017ab822e7865f305d4c01b23c6ebc4787637
Dmitriy Borodko <dmitriyborodko@gmail.com>
2014-09-29 14:51:34 +0700

    mondeay 1

---
+++ Simple_VK_Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple_VK_Client
+//
+//  Created by Dmitriy on 29/09/14.
+//  Copyright (c) 2014 ALS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Simple_VK_Client/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Simple_VK_Client
+//
+//  Created by Dmitriy on 29/09/14.
+//  Copyright (c) 2014 ALS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
